bb21bf9158bc818b8215bcaa93c80c94837145a2
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2022-08-31 11:19:15 +0545

    creat Todo component with css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
-import City from './components/City';
+import Todo from './components/Todo';
+// import City from './components/City';
 
 function App() {
   return (
     <div className="App">
-      <City city="London" feature="beautiful"/>
+      {/* <City city="London" feature="beautiful"/>
       <City city="Kathmandu" feature="awesome"/>
       <City city="Beijing" feature="great"/>
-      <City city="Delhi" feature="marvelous"/>
+      <City city="Delhi" feature="marvelous"/> */}
 
       {/* <p>Hello, I am in London. It is beautiful.</p>
       <p>Hello, I am in Kathmandu. It is awesome.</p>
       <p>Hello, I am in Beijing. It is great.</p>
       <p>Hello, I am in Delhi. It is marvelous.</p> */}
+
+      <Todo/>
     </div>
   );
 }
---
+++ src/components/Todo.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Todo.css'
+
+const Todo = () => {
+  return (
+    <div className='todo'>
+        <div className="todo-logo">
+            <h3>Todo App</h3>
+        </div>
+
+        <div className="todo-form">
+            <input type="text" className="input" placeholder='Add items' />
+            <input type="button" className="button" value="Add Todo" />
+        </div>
+
+        <div className="todo-lists">
+            <h3>Task Lists</h3>
+            <ul>
+                <li>Walk for fresh.</li>
+                <li>Rest a while.</li>
+                <li>Feed a dog.</li>
+                <li>Watch a movie.</li>
+            </ul>
+
+            <button name='removeall' value="Remove All"/>
+        </div>
+    </div>
+  )
+}
+
+export default Todo
