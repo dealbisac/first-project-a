4b761a1f5e265a5374dd7f937012afb8a9185c30
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2022-09-02 11:31:56 +0545

    complete localstorage todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,16 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Todo.css'
 
 const Todo = () => {
     const [inputData, setInputData] = useState('')
-    const [todos, setTodos] = useState([
-        'Walk for fresh.',
-        'Rest a while.',
-        'Feed a dog.',
-        'Watch a movie.'
-    ])
+    const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('lists')) || []);
 
     console.log(inputData);
+
+    //use effect to set the todos array to local storage
+    useEffect(() => {
+        localStorage.setItem('lists', JSON.stringify(todos));
+      }, [todos]);
+
+    // use effect to fetch data from local storage
+    useEffect(() => {
+        const data = JSON.parse(localStorage.getItem('lists'));
+        if (data) {
+            setTodos(data);
+        }
+    } , [])
 
     // Create: Add new items(tasks) to the existing todo array.
     // Arrow function can also be used
@@ -66,25 +74,24 @@ const Todo = () => {
             <div className="todo-lists">
                 <h3>Task Lists</h3>
                 <ul>
-                    {
-                        todos.map((todo) => (
-                            <li>
-                                {todo}
-                                <span
-                                    className="delete"
-                                    onClick={() => deleteTodo(todos.indexOf(todo))}
-                                >
-                                    X
-                                </span>
-                                <span
-                                    className='update'
-                                    onClick={() => updateTodo(todos.indexOf(todo))}
-                                >
-                                    U
-                                </span>
-                            </li>
-                        ))
-                    }
+
+                    {todos.map((todo, index) => (
+                        <li key={index}>
+                            <span>{todo}</span>
+                            <input
+                                type="button"
+                                className="update"
+                                value="Update"
+                                onClick={() => updateTodo(index)}
+                            />
+                            <input
+                                type="button"
+                                className="delete"
+                                value="Delete"
+                                onClick={() => deleteTodo(index)}
+                            />  
+                        </li>
+                    ))}
                 </ul>
             </div>
 
